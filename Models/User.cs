e4c6342ef6099941ceb0b357a5dfb4020225b663
ElianaBeltran <88954859+ElianaBeltran@users.noreply.github.com>
2026-02-11 21:33:23 -0400

    CRUD
    ASP.NET MVC

---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD.Models
+{
+    public class User
+    {
+        [Key]
+        [Display (Name ="User ID")]
+        public int UserId { get; set; }
+
+        [Required]
+        [Display(Name = "User Name")]
+        public string Name { get; set; }
+
+        
+        public string Email { get; set; }
+
+    }
+}
